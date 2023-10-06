bcd24c949d4cb74cad244d5ee3d01b740928ee68
shreya <shreya@shreyas-MacBook-Air.local>
2023-10-06 13:01:20 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <h1
+      className="text-3xl font-bold underline text-center p-10
+  "
+    >
+      Hello world!
+    </h1>
+  );
+}
+
+export default App;
